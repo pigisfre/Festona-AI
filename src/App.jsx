ed2c90108b3f12ecb78ccfce03f3299a681cc801
pigisfre <pigisfre@gmail.com>
2024-11-07 15:00:45 +0000

    vediamo i camb

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,21 @@ import './App.css';
 
 
 const App = () => {
+
+  // Esegui questo codice quando il componente viene montato
+React.useEffect(() => {
+  const handleResize = () => {
+    // Rileva l'altezza della finestra e adatta il layout in base a essa
+    document.body.style.height = `${window.innerHeight}px`;
+  };
+
+  // Aggiungi l'evento di ridimensionamento
+  window.addEventListener('resize', handleResize);
+
+  // Rimuovi l'evento quando il componente viene smontato
+  return () => window.removeEventListener('resize', handleResize);
+}, []);
+
   return (
     <div className="App">
       <h1>F.E.S.T.O.N.A</h1>
